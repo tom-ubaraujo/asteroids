93a6c6f0b534634ac6cbdb03f06a2adbfe11797f
Tom Araúo <ubaraujo@protonmail.com>
2024-09-10 01:26:27 -0300

    done with player sprite and methods

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
-    pygame.init
-    print("Starting asteroids!")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
@@ -13,7 +21,15 @@ def main():
                 return
 
         screen.fill(BLACK_COLOR)
+
+        for d in drawable:
+            d.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
